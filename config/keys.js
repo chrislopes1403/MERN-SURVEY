02dcd8dced56640fa95bfb75c2ac39adc9ebd9d6
Chris <chrislopes1403@comcast.net>
2021-02-11 19:53:46 -0500

    production setup

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports={
+    googleClientID:'877234901180-0cu60un8ocjua3bm52img02rq7hlu7qi.apps.googleusercontent.com',
+    googleClientSecret:'aca2o0lK0j7zPV6BVjL-Mqcb',
+    mongoURI:"mongodb+srv://Chris:lJk7aA7UQZg8vrwF@cluster0.toq40.mongodb.net/MernSurvey?retryWrites=true&w=majority",
+    //mongoURI:'mongodb://Chris:lJk7aA7UQZg8vrw@localhost:8080/MernSurvey?authSource=admin'
+    cookieKey:"p9WWImrQMihvqBqGBa4qJ9V2GHptaIK2"
+};
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
 
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected!");
+}).catch(err => console.log(err));
 
 const app =express();
 
+app.use(
+    cookieSession({
+        maxAge: 1 *24 *60 *60 *1000,  // 1 day / 24 hours / 60 min / 60 sec / 1000 milsec
+        keys:[keys.cookieKey]
+    })
+);
 
-app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const r = require('randomstring');
+
+console.log(r.generate(64));
 
 const PORT= process.env.PORT || 8080;
 app.listen(PORT);
